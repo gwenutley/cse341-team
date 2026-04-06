6de3ea0ac10101869036aaa03eee01d317d2c5df
Rowland Momoh <momohrowland@gmail.com>
2026-04-06 06:03:51 +0100

    Chore: Prune unnecessary packages and lines

--- database/index.js
+++ database/index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-/* eslint-disable no-console */
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI)
--- routes/food.route.js
+++ routes/food.route.js
@@ -1,5 +1,4 @@
-const express = require('express')
-const router = express.Router()
+const router = require('express').Router()
 const foodController = require('../controllers/food.controller')
 
 router.get('/', foodController.getAll)
