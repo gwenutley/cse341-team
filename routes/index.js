201a9978de41ceb41fe7c4dabfed9296260e6f6e
Rowland Momoh <momohrowland@gmail.com>
2026-04-09 01:03:02 +0100

    Add welcome message to root route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
 
+router.get('/', (_req, res) => {
+  res.send(`
+    <h1>Welcome to the CSE341 Team API</h1>
+    <p>Please visit the <a href="/api-docs">API Documentation</a> to view the full docs and available endpoints.</p>
+  `)
+})
 router.use('/foods', require('./food.route'))
 router.use('/users', require('./user.route'))
 
