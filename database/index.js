a48e610d7c7b6cc80656e5fcd27454e50c5a9179
Rowland Momoh <momohrowland@gmail.com>
2026-04-11 13:09:36 +0100

    Add tests setup and packages and update database connection methods

--- database/index.js
+++ database/index.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+    const uri =
+      process.env.NODE_ENV === 'test'
+        ? process.env.MONGO_TEST_URI
+        : process.env.MONGO_URI
+    await mongoose.connect(uri)
     console.log('MongoDB connected')
   } catch (err) {
     console.error(err.message)
@@ -10,4 +14,27 @@ const connect = async () => {
   }
 }
 
-module.exports = { connect }
+const close = async () => {
+  try {
+    await mongoose.connection.close()
+    console.log('MongoDB connection closed')
+  } catch (err) {
+    console.error(err.message)
+    throw err
+  }
+}
+
+const clear = async () => {
+  try {
+    const collections = mongoose.connection.collections
+    for (const key in collections) {
+      await collections[key].deleteMany()
+    }
+    console.log('MongoDB collections cleared')
+  } catch (err) {
+    console.error(err.message)
+    throw err
+  }
+}
+
+module.exports = { connect, close, clear }
